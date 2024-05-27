19b67894a7443e5fc4214514b97f95a118c6a7e7
lucas <zaratinlucas@gmail.com>
2024-05-27 11:44:10 -0300

    Create a banck account in the terminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception   {
+    
+        Scanner scanner = new Scanner(System.in);
+  
+        System.out.println("Digite o numero da conta");
+        int conta = scanner.nextInt();
+
+        System.out.println("Digite o nome da Agência");
+        String agencia = scanner.next();
+
+        scanner.nextLine();
+
+        System.out.println("Digite o nome do Cliente");
+        String cliente = scanner.nextLine();
+
+        System.out.println("Digite o Saldo do Cliente");
+        Double saldo = scanner.nextDouble();   
+        
+        scanner.close();
+
+        System.out.println("Ola " + cliente + ", obrigado por criar uma conta em nosso banco, sua agencia é " + agencia + ", conta " + conta + 
+        ", conta " + conta + ", e seu saldo " + saldo + " já esta disponivel para saque" );
     }
 }
